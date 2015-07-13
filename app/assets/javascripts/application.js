fbaed58c8703645231e0d9db31cbbb0d325b35c2
mckshreder <garrettmckenzie@ymail.com>
2015-07-12 23:10:01 -0700

    nave bar has dropdowns functioning, they are not clean styled or aligned.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,13 +33,45 @@ $(" #topright_nav li").hover(function(){
 });
 
 
-// this is the dropdown effects for the main menu bars on every page
-$('nav li ul').hide().removeClass('fallback');
-$('nav li').hover(
-  function () {
-    $('ul', this).stop().slideDown(100);
-  },
-  function () {
-    $('ul', this).stop().slideUp(100);
-  }
-);
+var timeout	= 500;
+var closetimer	= 0;
+var ddmenuitem	= 0;
+
+// open hidden layer
+function mopen(id)
+{	
+	// cancel close timer
+	mcancelclosetime();
+
+	// close old layer
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+
+	// get new layer and show it
+	ddmenuitem = document.getElementById(id);
+	ddmenuitem.style.visibility = 'visible';
+
+}
+// close showed layer
+function mclose()
+{
+	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+}
+
+// go close timer
+function mclosetime()
+{
+	closetimer = window.setTimeout(mclose, timeout);
+}
+
+// cancel close timer
+function mcancelclosetime()
+{
+	if(closetimer)
+	{
+		window.clearTimeout(closetimer);
+		closetimer = null;
+	}
+}
+
+// close layer when click-out
+document.onclick = mclose;

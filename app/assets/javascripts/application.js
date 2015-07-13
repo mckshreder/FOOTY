fca912bc9a0c4e55d8843f45213a62bfdfd95e5b
mckshreder <garrettmckenzie@ymail.com>
2015-07-13 00:44:39 -0700

    the code is working for the nav bar but looking to to using css instead of java script...

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,45 +35,45 @@ $(" #topright_nav li").hover(function(){
 
 
 // this is for the nav bar
-var timeout	= 500;
-var closetimer	= 0;
-var ddmenuitem	= 0;
+// var timeout	= 500;
+// var closetimer	= 0;
+// var ddmenuitem	= 0;
 
-// open hidden layer
-function mopen(id)
-{	
-	// cancel close timer
-	mcancelclosetime();
+// // open hidden layer
+// function mopen(id)
+// {	
+// 	// cancel close timer
+// 	mcancelclosetime();
 
-	// close old layer
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+// 	// close old layer
+// 	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
 
-	// get new layer and show it
-	ddmenuitem = document.getElementById(id);
-	ddmenuitem.style.visibility = 'visible';
+// 	// get new layer and show it
+// 	ddmenuitem = document.getElementById(id);
+// 	ddmenuitem.style.visibility = 'visible';
 
-}
-// close showed layer
-function mclose()
-{
-	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
-}
+// }
+// // close showed layer
+// function mclose()
+// {
+// 	if(ddmenuitem) ddmenuitem.style.visibility = 'hidden';
+// }
 
-// go close timer
-function mclosetime()
-{
-	closetimer = window.setTimeout(mclose, timeout);
-}
+// // go close timer
+// function mclosetime()
+// {
+// 	closetimer = window.setTimeout(mclose, timeout);
+// }
 
-// cancel close timer
-function mcancelclosetime()
-{
-	if(closetimer)
-	{
-		window.clearTimeout(closetimer);
-		closetimer = null;
-	}
-}
+// // cancel close timer
+// function mcancelclosetime()
+// {
+// 	if(closetimer)
+// 	{
+// 		window.clearTimeout(closetimer);
+// 		closetimer = null;
+// 	}
+// }
 
-// close layer when click-out
-document.onclick = mclose;
+// // close layer when click-out
+// document.onclick = mclose;

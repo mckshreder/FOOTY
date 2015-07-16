1f8936307aa36c37d9ec99fdf0b28e0e13c1a620
mckshreder <garrettmckenzie@ymail.com>
2015-07-15 22:33:56 -0700

    this is the commit before I add google maps api to get user locatoin for post and event creation page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716025919) do
+ActiveRecord::Schema.define(version: 20150716043943) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,6 @@ ActiveRecord::Schema.define(version: 20150716025919) do
     t.string   "age"
     t.string   "location"
     t.string   "bio"
-    t.float    "latitude"
-    t.float    "longitude"
     t.string   "password"
   end
 
